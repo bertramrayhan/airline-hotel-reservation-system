608b7049e341117f64692e6d15fd5f305dd81ae8
Replit user <>
2023-06-16 13:53:22 +0000

    main

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ class Hotel:
     self.location = location
     self.rating = rating
     self.total_rooms = total_rooms
+    self.rooms = []
 
   def __repr__(self):
     return f"Hotel name : {self.name}, Location = {self.location}, Rating : {self.rating}, Total rooms : {self.total_rooms}"
+
+  def add_rooms(self,room):
+    self.rooms.append(room)
+
+class Room:
+  def __init__(self, number, type, price, availability):
+    self.number = number
+    self.type = type
+    self.price = price
+    self.availability = availability
+
+    
